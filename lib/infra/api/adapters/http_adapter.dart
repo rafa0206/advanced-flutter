b9d1f46510c2326d9ba27e2703a63457f1de7970
rafa0206 <rafaelfernandes0206@gmail.com>
2025-04-19 21:46:51 -0300

    ensure HttpAdapter accept non string params

--- lib/infra/api/adapters/http_adapter.dart
+++ lib/infra/api/adapters/http_adapter.dart
@@ -16,7 +16,7 @@ final class HttpAdapter implements HttpGetClient {
   Future<T?> get<T>({
     required String url,
     Json? headers,
-    Map<String, String?>? params,
+    Json? params,
     Map<String, String>? queryString,
   }) async {
     final response = await client.get(
@@ -48,8 +48,8 @@ final class HttpAdapter implements HttpGetClient {
     return defaultHeaders..addAll({ for (final key in (headers ?? {}).keys) key: headers![key].toString() });
   }
 
-  Uri _buildUri({required String url, Map<String, String?>? params, Map<String, String>? queryString}) {
-    url = params?.keys.fold(url, (result, key) => result.replaceFirst(':$key', params[key] ?? '')).removeSuffix('/') ?? url;
+  Uri _buildUri({required String url, Json? params, Map<String, String>? queryString}) {
+    url = params?.keys.fold(url, (result, key) => result.replaceFirst(':$key', params[key]?.toString() ?? '')).removeSuffix('/') ?? url;
     url = queryString?.keys.fold('$url?', (result, key) => '$result$key=${queryString[key]}&').removeSuffix('&') ?? url;
     return Uri.parse(url);
   }
