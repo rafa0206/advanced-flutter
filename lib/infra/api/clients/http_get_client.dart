37458f39d7e270fb1366237f5de539c5568c50c2
rafa0206 <rafaelfernandes0206@gmail.com>
2025-04-19 21:58:04 -0300

    change HttpGetClient to accept non string params, headers and qs

--- lib/infra/api/clients/http_get_client.dart
+++ lib/infra/api/clients/http_get_client.dart
@@ -1,8 +1,10 @@
+import 'package:advanced_flutter/infra/types/json.dart';
+
 abstract interface class HttpGetClient {
   Future<T?> get<T>({
     required String url,
-    Map<String, String>? headers,
-    Map<String, String?>? params,
-    Map<String, String>? queryString,
+    Json? headers,
+    Json? params,
+    Json? queryString,
   });
 }
