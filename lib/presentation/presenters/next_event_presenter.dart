e09cfcd854f9be34d4cd54e42fb86396f35e3844
rafa0206 <rafaelfernandes0206@gmail.com>
2025-05-04 22:38:03 -0300

    ensure NextEventRxPresenter calls nextEventLoader on load

--- lib/presentation/presenters/next_event_presenter.dart
+++ lib/presentation/presenters/next_event_presenter.dart
@@ -1,7 +1,7 @@
 abstract class NextEventPresenter {
   Stream<NextEventViewModel> get nextEventStream;
   Stream<bool> get isBusyStream;
-  void loadNextEvent({ required String groupId, bool isReload });
+  Future<void> loadNextEvent({ required String groupId, bool isReload });
 }
 
 final class NextEventViewModel {
