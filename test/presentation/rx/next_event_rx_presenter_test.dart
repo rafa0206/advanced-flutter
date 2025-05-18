2d544cf030ddfad1e5d4f6fab560df93395becad
rafa0206 <rafaelfernandes0206@gmail.com>
2025-05-18 07:55:59 -0300

    build players list correctly

--- test/presentation/rx/next_event_rx_presenter_test.dart
+++ test/presentation/rx/next_event_rx_presenter_test.dart
@@ -33,10 +33,19 @@ final class NextEventRxPresenter {
   }
 
   NextEventViewModel _mapEvent(NextEvent event) => NextEventViewModel(
-      doubt: event.players.where((player) => player.confirmationDate == null).sortedBy((player) => player.name).map(_mapPlayer).toList(),
-      out: event.players.where((player) => player.confirmationDate != null && !player.isConfirmed).sortedBy((player) => player.confirmationDate!).map(_mapPlayer).toList(),
-      goalkeepers: event.players.where((player) => player.confirmationDate != null && player.isConfirmed && player.position == 'goalkeeper').sortedBy((player) => player.confirmationDate!).map(_mapPlayer).toList()
-  );
+        doubt: event.players.where((player) => player.confirmationDate == null).sortedBy((player) => player.name).map(_mapPlayer).toList(),
+        out: event.players.where((player) => player.confirmationDate != null && !player.isConfirmed).sortedBy((player) => player.confirmationDate!).map(_mapPlayer).toList(),
+        goalkeepers: event.players
+            .where((player) => player.confirmationDate != null && player.isConfirmed && player.position == 'goalkeeper')
+            .sortedBy((player) => player.confirmationDate!)
+            .map(_mapPlayer)
+            .toList(),
+        players: event.players
+            .where((player) => player.confirmationDate != null && player.isConfirmed && player.position != 'goalkeeper')
+            .sortedBy((player) => player.confirmationDate!)
+            .map(_mapPlayer)
+            .toList(),
+      );
 
   NextEventPlayerViewModel _mapPlayer(NextEventPlayer player) => NextEventPlayerViewModel(
         name: player.name,
@@ -204,7 +213,14 @@ void main() {
   });
 
   test('should map goalkeeper', () async {
-    final player = NextEventPlayer(id: anyString(), name: anyString(), isConfirmed: true, photo: anyString(), position: 'goalkeeper', confirmationDate: anyDate());
+    final player = NextEventPlayer(
+      id: anyString(),
+      name: anyString(),
+      isConfirmed: true,
+      photo: anyString(),
+      position: 'goalkeeper',
+      confirmationDate: anyDate(),
+    );
     nextEventLoader.simulatePlayers([player]);
     sut.nextEventStream.listen((event) {
       expect(event.goalkeepers[0].name, player.name);
@@ -212,6 +228,23 @@ void main() {
       expect(event.goalkeepers[0].isConfirmed, player.isConfirmed);
       expect(event.goalkeepers[0].photo, player.photo);
       expect(event.goalkeepers[0].position, player.position);
+    });
+    await sut.loadNextEvent(groupId: groupId);
+  });
+
+  test('should build players list sorted by confirmation date', () async {
+    nextEventLoader.simulatePlayers([
+      NextEventPlayer(id: anyString(), name: 'C', isConfirmed: true, confirmationDate: DateTime(2024, 1, 1, 10), position: 'goalkeeper'),
+      NextEventPlayer(id: anyString(), name: 'A', isConfirmed: anyBool()),
+      NextEventPlayer(id: anyString(), name: 'B', isConfirmed: true, confirmationDate: DateTime(2024, 1, 1, 11), position: 'defender'),
+      NextEventPlayer(id: anyString(), name: 'E', isConfirmed: false, confirmationDate: DateTime(2024, 1, 1, 9)),
+      NextEventPlayer(id: anyString(), name: 'D', isConfirmed: true, confirmationDate: DateTime(2024, 1, 1, 12)),
+      NextEventPlayer(id: anyString(), name: 'F', isConfirmed: true, confirmationDate: DateTime(2024, 1, 1, 8), position: 'goalkeeper')
+    ]);
+    sut.nextEventStream.listen((event) {
+      expect(event.players.length, 2);
+      expect(event.players[0].name, 'B');
+      expect(event.players[1].name, 'D');
     });
     await sut.loadNextEvent(groupId: groupId);
   });
