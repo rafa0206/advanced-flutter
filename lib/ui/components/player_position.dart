4f3f19d0bdf67cea82e0ee1f4c7199071293536a
rafa0206 <rafaelfernandes0206@gmail.com>
2025-05-25 17:26:56 -0300

    improve layout

--- lib/ui/components/player_position.dart
+++ lib/ui/components/player_position.dart
@@ -1,23 +1,24 @@
+import 'package:awesome_flutter_extensions/awesome_flutter_extensions.dart';
 import 'package:flutter/material.dart';
 
 final class PlayerPosition extends StatelessWidget {
   final String? position;
 
-  const PlayerPosition({
-    this.position,
-    super.key
-  });
+  const PlayerPosition({this.position, super.key});
 
   String buildPositionLabel() => switch (position) {
-    'goalkeeper' => 'Goleiro',
-    'defender' => 'Zagueiro',
-    'midfielder' => 'Meia',
-    'forward' => 'Atacante',
-    _ => 'Gandula'
-  };
+        'goalkeeper' => 'Goleiro',
+        'defender' => 'Zagueiro',
+        'midfielder' => 'Meia',
+        'forward' => 'Atacante',
+        _ => 'Gandula',
+      };
 
   @override
   Widget build(BuildContext context) {
-    return Text(buildPositionLabel());
+    return Text(
+      buildPositionLabel(),
+      style: context.textStyles.labelMedium.apply(color: context.colors.scheme.primary.withOpacity(0.7)),
+    );
   }
 }
