b3d303fe125ef717305d68e49a842d467b1881e5
rafa0206 <rafaelfernandes0206@gmail.com>
2025-07-31 20:17:52 -0300

    ensure page show cache data

--- test/e2e/next_event_page_test.dart
+++ test/e2e/next_event_page_test.dart
@@ -136,4 +136,21 @@ void main() {
     await tester.pump();
     expect(find.text('Claudio Gamarra'), findsOneWidget);
   });
+
+  testWidgets('should present cache data', (tester) async {
+    client.simulateServerError();
+    cacheManager.file.simulateResponse(responseJson);
+    await tester.pumpWidget(sut);
+    await tester.pump();
+    await tester.ensureVisible(find.text('Rafael Fernandes', skipOffstage: false));
+    await tester.pump();
+    expect(find.text('Rafael Fernandes'), findsOneWidget);
+    await tester.ensureVisible(find.text('Lionel Messi', skipOffstage: false));
+    await tester.pump();
+    expect(find.text('Lionel Messi'), findsOneWidget);
+    await tester.ensureVisible(find.text('Claudio Gamarra', skipOffstage: false));
+    await tester.pump();
+    expect(find.text('Claudio Gamarra'), findsOneWidget);
+  });
+
 }
