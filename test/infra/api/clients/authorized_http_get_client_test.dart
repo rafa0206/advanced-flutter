55d256bc475075c9ee31cf1d587f7cc5e6fae258
rafa0206 <rafaelfernandes0206@gmail.com>
2025-08-19 21:52:43 -0300

    ensure AuthorizedHttpGetClient rethrow on HttpClient error

--- test/infra/api/clients/authorized_http_get_client_test.dart
+++ test/infra/api/clients/authorized_http_get_client_test.dart
@@ -102,4 +102,11 @@ void main() {
     expect(future, throwsA(error));
   });
 
+  test('should rethrow on HttpClient error', () async {
+    final error = Error();
+    httpClient.error = error;
+    final future = sut.get(url: url);
+    expect(future, throwsA(error));
+  });
+
 }
